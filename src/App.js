463d98ba58df1877c73b18dcb53d328515a13c6a
yitimo <admin@yitimo.com>
2019-04-07 12:05:32 +0800

    update sample

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  swTheme(color) {
+    window.less.modifyVars({
+      '@primary-color': color
+    });
+  }
   render() {
     return (
-      <div className="App">
+      <div className="App primary">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -19,6 +25,8 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Button onClick={() => this.swTheme('#ffea00')}>#ffea00</Button>
+          <Button onClick={() => this.swTheme('#39C5BB')}>#39C5BB</Button>
         </header>
       </div>
     );
